1810062b9d49ae2bbec73aa0101193ae9ca9310b
nipun1a <nipun1a1b@gmail.com>
2025-06-04 23:18:14 +0530

    practice a lot

--- server_node.js
+++ server_node.js
@@ -4,6 +4,24 @@ const port = 3000;
 
 // create an HTTP server
 
-const server = http.createServer((req, res)) => {
-    
-}
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello, World!\n');
+    }else if (req.url === '/about'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('About Page\n');}
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('404 Not Found\n');
+    }
+});
+
+// start the server
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
+
