a1116f9c905ad9d843783cb669b907db7bac2bf2
nipun1a <nipun1a1b@gmail.com>
2025-06-06 23:17:37 +0530

    backend with express

---
+++ express/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+app.get('/about', (req, res) => {
+    res.send("About Page");
+});
+app.use((req, res) => {
+    res.status(404).send("404 Not Found");
+});
+
+app.listen(port,() => {
+    console.log(`server running at http://localhost:${port}/`);
+
+}); 
