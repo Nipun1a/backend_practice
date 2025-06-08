9b107e86cd829c5c4af8a95964f590d8b2757c24
nipun1a <nipun1a1b@gmail.com>
2025-06-08 13:56:44 +0530

    nodemon is working now and i leaerned how to use  it

--- express/index.js
+++ express/index.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.send("About Page");
 });
+app.get("/twitter", (req, res) => {
+    res.send("Twitter Page");
+});
 app.use((req, res) => {
     res.status(404).send("404 Not Found");
 });
@@ -17,3 +20,6 @@ app.listen(port,() => {
 
 }); 
 // i am dealing with the issue in which i am not able to run the server with the help of nodemon  tommorrow i will try to resolve the issue and run the server with the help of nodemon
+
+// now nodemon is working fine and i am able to run the server with the help of nodemon
+
